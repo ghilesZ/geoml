3c4786a30224ff34d717d54fff6039abe461f1c1
remyzorg <besognet.remy@gmail.com>
2015-10-21 18:14:33 +0200

    non-regular polygon : area + polymeter

--- src/polygon.ml
+++ src/polygon.ml
@@ -3,15 +3,23 @@ type t = Point.t list
 
 let make l = l
 
-let perimeter =
+let fold f acc p =
   let rec aux acc = function
   | [] -> acc
-  | [pt] -> acc
+  | [pt] -> f acc pt @@ List.hd p
   | pt1 :: pt2 :: pg ->
-    aux (acc +. Point.distance pt1 pt2) (pt2 :: pg)
-  in aux 0.
+    aux (f acc pt1 pt2) (pt2 :: pg)
+  in aux acc p
 
-let area p = assert false
+let perimeter p =
+  fold Point.(fun acc p1 p2 ->
+      acc +. distance p1 p2
+    ) 0. p
+
+let area p =
+  fold Point.(fun acc p1 p2 ->
+      acc +. (p1.x *. p2.y -. p1.y *. p2.x)
+    ) 0. p /. 2.
 
 module Regular = struct
 
@@ -30,17 +38,6 @@ module Regular = struct
       Point.distance center (Point.barycenter [fst; snd])
   }
 
-  let perimeter rp = float_of_int rp.edges *. rp.len
-  let area rp = rp.len *. rp.apothem /. 2. *. float_of_int rp.edges
-
-  let translate rp dx dy =
-    { rp with
-      center = Point.translate rp.center dx dy;
-      fst = Point.translate rp.fst dx dy
-    }
-
-  let is_square rp = rp.edges = 4
-
   let next_point ?(nth=0) rp pt =
     if nth = rp.edges then rp.center
     else
@@ -52,15 +49,33 @@ module Regular = struct
       else if not @@ filter current next then acc
       else
         let nth = nth + 1 in
-        aux (nth + 1) (f nth current next) next (next_point ~nth rp next)
+        aux (nth + 1) (f nth acc current next) next (next_point ~nth rp next)
     in aux 0 acc rp.fst (next_point rp rp.fst)
+
+  let perimeter rp = float_of_int rp.edges *. rp.len
+  let area rp = rp.len *. rp.apothem /. 2. *. float_of_int rp.edges
+
+  let to_polygon rp =
+    rp fold_filter (fun _ _ -> true)
+      (fun _ acc current next ->
+         next :: acc
+      ) [] rp
+
+
+  let translate rp dx dy =
+    { rp with
+      center = Point.translate rp.center dx dy;
+      fst = Point.translate rp.fst dx dy
+    }
+
+  let is_square rp = rp.edges = 4
 
   let contains rp pt =
     fold_filter
       (fun current next ->
          (0. > Point.determinant current next rp.center)
          = (0. > Point.determinant current next pt)
-      ) (fun nth current next -> nth = rp.edges) true rp
+      ) (fun nth _ current next -> nth = rp.edges) true rp
 
 
 end
