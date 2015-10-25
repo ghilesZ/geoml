0b51601b0733141d7d9ae7db3f479944b3353020
ghilesZ <ghiles.ziat@gmail.com>
2015-10-25 12:08:30 +0100

    fix bounding circle

--- src/circle.ml
+++ src/circle.ml
@@ -11,7 +11,10 @@ let radius ((_,r):t) = r
 let translate ((c,r):t) dx dy = 
   make (Point.translate c dx dy) r
 
-let contains ((c,r):t) p = (Point.sq_distance c p) < (r*.r)
+let contains ((c,r):t) p =
+  let d1 = Point.sq_distance c p
+  and d2 = (r*.r) in
+  d1 < d2
 
 let area ((_,r):t) = pi *. r *. r
 
@@ -32,17 +35,17 @@ let line_intersection ((c,r):t) (l:Line.t) =
     let l_2 = Line.translate l (-.cx) (-.cy) in
     let a = Line.get_coeff l_2 and b = Line.get_ord l_2 in
     (* we solve the equation at the origin for x*)
-    (* x² + y² = r²
-       x² + (ax + b)² = r²
-       x² + a²x² + 2abx + b² = r²
-       (a²+1)x² + 2abx + b²-r² = 0 *)
+    (* x² + y² = r²   and   y = ax+b  
+       => x² + (ax + b)² = r²
+       => x² + a²x² + 2abx + b² = r²
+       => (a²+1)x² + 2abx + b²-r² = 0 *)
     Math.solve (a*.a+.1.) (2.*.a*.b) (b*.b -. r*.r)
      (* we calculate the associated y*)
     |> List.map (fun x -> Point.make x (a*.x+.b))
      (* we translate the result to the first coordinates*)
     |> List.map (fun x -> Point.translate x cx cy)
-  | Line.X(x) ->
-     let a,b = c.Point.x,c.Point.y in
+  | Line.X(x) -> failwith "not implementd yet"
+     (*let a,b = c.Point.x,c.Point.y in
      let c = r*.r -. b*.b -. x*.x -. 2.*.a*.x +. a*.a
      in let delta = 4. *. (b*.b -. c) in
 	if delta < 0. then [] else 
@@ -50,7 +53,7 @@ let line_intersection ((c,r):t) (l:Line.t) =
 	  if delta = 0. then [Point.make x sol1]
 	  else let sol2 = (2. *. b +. (sqrt delta)) /. 2. in
 	       [Point.make x sol1;
-		Point.make x sol2]
+		Point.make x sol2]*)
 
 let intersection (((c1,_) as c):t) (((c2,_)as c'):t) = 
   let c1_c2 = Line.of_points c1 c2 in
@@ -77,35 +80,42 @@ let incircle p1 p2 p3 =
     (2. *. sqrt (s *. (s-.a) *. (s-.b) *.(s-.c))) /. (a+.b+.c)
   in make center radius
 
-let of_diameter p1 p2 = make (Point.center p1 p2) (0.5 *. Point.distance p1 p2)
+let of_diameter p1 p2 =
+  let c = Point.center p1 p2 in
+  let radius = (Point.distance p1 c) in
+  make c radius
      
 (** given a list of point, returns the smallest circle that
-   contains all the points of the list *)
+   contains all the points of the list, using emo welzl's algorithm.
+    complexity in expected linear time*)
 let bounding (pts : Point.t list) : t =
   let of_two x y pt = 
-    let c = of_diameter pt x in
-    if contains c y then ([x;pt],c) else
-      let c = of_diameter pt y in
-      if contains c x then ([y;pt],c)
-      else raise Not_found 
+    let cx = of_diameter pt x in
+    if contains cx y then ([x;pt],cx) else
+      let cy = of_diameter pt y in
+      if contains cy x then ([y;pt],cy)
+      else raise Not_found
   in
   let of_three x y z pt =
+    let try_2 a b contained = 
+      let c = (of_two a b pt) in
+      if contains (snd c) contained then c
+      else raise Not_found
+    in
     try 
-      let (s,c) = of_two x y pt in
-      if contains c z then (s,c) else
-	let (s,c) = of_two x z pt in
-	if contains c y then (s,c) else
-	  let (s,c) = of_two y z pt in
-	   if contains c x then (s,c) 
-	   else raise Not_found
+      try (try_2 x y z) 
+      with Not_found -> 
+	try (try_2 x z y) 
+	with Not_found -> (try_2 y z x)
     with
+    (* the solution goes through three points *)
     | Not_found -> begin
       (* returns the indice of the smallest value *)
       let smallest a b c = 
 	let res = ref (a,0) in
 	if b < a then res := (b,1);
-	if c < fst (!res) then res := (c,2);
-	snd(!res)
+	if c < fst (!res) then 2
+	else snd (!res)
       in
       let ((_,r1) as c1) = circumscribed pt x y
       and ((_,r2) as c2) = circumscribed pt x z
@@ -113,13 +123,13 @@ let bounding (pts : Point.t list) : t =
       and s1 = [pt;x;y] and s2 = [pt;x;z] and s3 = [pt;y;z] in
       let res_array = [|(s1,c1);(s2,c2);(s3,c3)|] in
       (match ((contains c1 z),(contains c2 y),(contains c3 x)) with
-      |(true,true,true) -> res_array.(smallest r1 r2 r3)
-      |(_,true,true) -> res_array.(smallest (r3+.1.) r2 r3)
-      |(true,_,true) -> res_array.(smallest r1 (r1+.1.) r3)
-      |(true,true,_) -> res_array.(smallest r1 r2 (r2+.1.))
-      |(true,_,_) -> res_array.(0)
-      |(_,true,_) -> res_array.(1)
-      |(_,_,true) -> res_array.(2)
+      |(true,false,false) -> res_array.(0)
+      |(false,true,false) -> res_array.(1)
+      |(false,false,true) -> res_array.(2)
+      |(false,true,true)  -> res_array.(smallest (r3+.1.) r2 r3)
+      |(true,false,true)  -> res_array.(smallest r1 (r1+.1.) r3)
+      |(true,true,false)  -> res_array.(smallest r1 r2 (r2+.1.))
+      |(true,true,true)   -> res_array.(smallest r1 r2 r3)
       | _ -> assert false)
     end
   in 
@@ -127,7 +137,7 @@ let bounding (pts : Point.t list) : t =
     match set with
     | [x] -> [x;pt],(of_diameter x pt)
     | [x;y] -> 
-      (try of_two x y pt 
+      (try of_two x y pt
       with Not_found -> ([x;y;pt],circumscribed pt x y))
     | [x;y;z] -> of_three x y z pt
     | _ -> assert false  
@@ -135,9 +145,10 @@ let bounding (pts : Point.t list) : t =
   let rec mindisk circle set pts =
     match pts with
     | [] -> circle
-    | h::tl when contains circle h ->  mindisk circle set tl 
+    | h::tl when contains circle h ->  mindisk circle set tl
     | h::tl -> 
-      let (new_set,new_circle) = update set h in mindisk new_circle new_set tl
+      let (new_set,new_circle) = update set h in
+      mindisk new_circle new_set tl
   in
   match pts with
   | [] -> failwith "can't build a bounding circle with an empty list"
--- tests/t_bounding.ml
+++ tests/t_bounding.ml
@@ -3,18 +3,18 @@ open Utils
 
 module T = struct
   let size_x = 800. 
-  and size_y = 700. 
-  and size = 4
+  and size_y = 600.
+  and size = 30
   and title = "Calculating the bounding circle/rectangle/polygon of a point list"
 
   type t = Point.t list  
     
   let new_val () = list_make
-    (fun _ -> gen_point 70. (size_x-.70.) 70. (size_y-.70.))
+    (fun _ -> gen_point 30. (size_x-.30.) 30. (size_y-.30.))
     size 
       
   let frame v =
-    Drawing.draw_string 25 675 "Press 'r' to generate a new cloud" Graphics.black;
+    Drawing.draw_string 25 25 "Press 'r' to generate a new cloud" Graphics.black;
     List.iter (fun e -> 
       let c = Circle.make e 5. in 
       Drawing.fill_circle c Graphics.black
