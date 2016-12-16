94ffe6011019c6c8ce4b12749e6af714d2920c43
ghilesZ <ghiles.ziat@gmail.com>
2016-12-16 12:10:31 +0100

    add null vector to vector module

--- src/vector.ml
+++ src/vector.ml
@@ -6,20 +6,22 @@ type t =
 
 let make dx dy = {dx;dy}
 
+let null = make 0. 0.
+
 let x_coord (v:t) = v.dx
 
 let y_coord (v:t) = v.dy
 
-let of_points (a: Point.t) (b: Point.t) : t =  
+let of_points (a: Point.t) (b: Point.t) : t =
   make (b.Point.x-.a.Point.x) (b.Point.y-.a.Point.y)
 
 let magnitude ({dx;dy}:t) = sqrt (dx*.dx +. dy*.dy)
 
-let normalize (({dx;dy} as v):t) = 
+let normalize (({dx;dy} as v):t) =
   let m = magnitude v in
   {dx=dx/.m;dy=dy/.m}
 
-let rotation theta {dx;dy} = 
+let rotation theta {dx;dy} =
   { dx=dx *. cos theta -. dy *. sin theta
   ; dy=dx *. sin theta +. dy *. cos theta }
 
@@ -31,13 +33,13 @@ let determinant v1 v2 = v1.dx *. v2.dy -. v1.dy *. v2.dx
 
 let opposite v = scal_mult (-1.) v
 
-let add (v1:t) (v2:t) : t = make (v1.dx+.v2.dx) (v1.dy+.v2.dy) 
+let add (v1:t) (v2:t) : t = make (v1.dx+.v2.dx) (v1.dy+.v2.dy)
 
 let substract v1 v2 = opposite v2 |> add v1
 
 let move_to ({dx;dy}:t) = Point.translate dx dy
 
-let angle v1 v2 = 
+let angle v1 v2 =
   let v1 = normalize v1
   and v2 = normalize v2 in
   dot_product v1 v2 |> acos
--- src/vector.mli
+++ src/vector.mli
@@ -1,10 +1,12 @@
-(** 
+(**
     The module for the euclidean vector manipulation.
 *)
 type t = private {
   	dx : float;
   	dy : float;
-}
+           }
+
+val null : t
 
 val make : float -> float -> t
 
@@ -12,18 +14,20 @@ val x_coord : t -> float
 
 val y_coord : t -> float
 
+(** of_points a b returns the vector begining at a and landing at b *)
 val of_points : Point.t -> Point.t -> t
 
 val magnitude : t -> float
 
+(** return a vector with same direction but with a norm equal to 1. *)
 val normalize : t -> t
-  
+
 val rotation : float -> t -> t
 
-(** dot product of two vectors *)  
+(** dot product of two vectors *)
 val dot_product : t -> t -> float
 
-(** scalar product of two vectors*) 
+(** scalar product of two vectors*)
 val scal_mult : float -> t -> t
 
 val determinant : t -> t -> float
@@ -34,10 +38,10 @@ val add : t -> t -> t
 
 val substract : t -> t -> t
 
-(** move_to v p returns the translation of p according to the vector v *)  
+(** move_to v p returns the translation of p according to the vector v *)
 val move_to : t -> Point.t -> Point.t
 
-(** returns the angle between two vectors*)  
+(** returns the angle between two vectors*)
 val angle : t -> t -> float
 
 (** compute the angle in radians *)
