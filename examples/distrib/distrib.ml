5f2807cbba9ddf2bcf173f808fca1be2d6bbf53e
ziat <ghiles.ziat@gmail.com>
2019-03-17 22:37:34 -0400

    example++

---
+++ examples/distrib/distrib.ml
@@ -0,0 +1,72 @@
+open Geoml
+
+let iof = int_of_float
+let foi = float_of_int
+
+let list_make f s =
+  let rec aux res = function
+  | 0 -> res
+  | n -> aux ((f ())::res) (n-1)
+  in aux [] s
+
+(*************** Random utilities *******************)
+let gen_point xmin xmax ymin ymax =
+  let x = xmin +. (foi (Random.int (int_of_float (xmax-.xmin))))
+  and y = ymin +. (foi (Random.int (int_of_float (ymax-.ymin)))) in
+  Point.make x y
+
+let gen_triangle xmin xmax ymin ymax =
+  let p1 = gen_point xmin xmax ymin ymax
+  and p2 = gen_point xmin xmax ymin ymax
+  and p3 = gen_point xmin xmax ymin ymax
+  in Triangle.make p1 p2 p3
+
+(****************** Main example **********************)
+
+let size_x = 800.
+and size_y = 700.
+and title = "test of random distribution"
+
+let work (c,t,r) =
+  (list_make (fun () -> Circle.random_point c) 1000),
+  (list_make (fun () -> Triangle.random_point t) 1000),
+  (list_make (fun () -> Rectangle.random_point r) 1000)
+
+let new_val () =
+  let rect = Rectangle.make (Point.make 200. 200.) (size_x-.200.) (size_y -.200.) in
+  let c = Circle.make (Rectangle.random_point rect) 200.
+  and tri = gen_triangle 0. 800. 0. 700.
+  and r = Rectangle.make (Rectangle.random_point rect) 200. 300. in
+  c,tri,r
+
+let frame (c,tri,rect) =
+  Drawing.draw_string 25 675 "Press 'r' to generate new shapes" Graphics.black;
+  let pts1,pts2,pts3 = work (c,tri,rect) in
+  List.iter (fun e -> Drawing.draw_point e Graphics.green) pts1;
+  List.iter (fun e -> Drawing.draw_point e Graphics.red) pts2;
+  List.iter (fun e -> Drawing.draw_point e Graphics.magenta) pts3;
+  Drawing.draw_circle c Graphics.blue;
+  Drawing.draw_triangle tri Graphics.blue;
+  Drawing.draw_rectangle rect Graphics.blue
+
+
+(****** Window and event handling *****)
+let clear () = Drawing.fill_screen Graphics.white
+
+let handler status =
+  let open Graphics in
+  if status.key = 'r' then begin
+      clear ();
+      new_val () |> frame
+    end
+
+let loop () =
+  Graphics.loop_at_exit [Graphics.Key_pressed] handler
+
+let doit () =
+  Random.self_init ();
+  Drawing.open_graph size_x size_y title;
+  new_val () |> frame;
+  loop ()
+
+let () = doit()
--- test/check.ml
+++ test/check.ml
@@ -1,13 +1,13 @@
 (* This modules implements verification test over the library *)
+open Geoml
 
 let segment() =
-  let open Geoml in
   let exn = Invalid_argument "Segment.make: points should be different" in
   Alcotest.check_raises "same point" exn (fun () -> ignore (Segment.make Point.orig Point.orig))
 
 let constructors =
-  [ "Segment", `Quick, segment]
+  ["Segment", `Quick, segment]
 
 let () =
   Alcotest.run "test suite"
-    [ "constructors"     , constructors]
+    ["constructors"     , constructors]
