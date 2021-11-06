6729b74b2d93c483d4893649e7aea66e8b95a61f
ziat <ghiles.ziat@gmail.com>
2021-11-06 14:12:09 +0100

    Line.make raise error when a=b=0.

--- src/line.ml
+++ src/line.ml
@@ -1,143 +1,129 @@
-type t = X of float | Y of float * float (** linear equation type *)
+type t = X of float | Y of float * float  (** linear equation type *)
 
 let print fmt = function
-  | Y(a,b) -> Format.fprintf fmt "y=%fx +. %f" a b
-  | X(c) -> Format.fprintf fmt "x=%f" c
+  | Y (a, b) -> Format.fprintf fmt "y=%fx +. %f" a b
+  | X c -> Format.fprintf fmt "x=%f" c
 
 type error = Parallel of t * t | Same_coordinates of Point.t
 
 exception Error of error
 
-let print_error fmt e =
-  let open Format in
-  match e with
-  | Parallel(l1,l2) -> fprintf fmt "Bad arguments : parallel lines %a and %a" print l1 print l2
-  | Same_coordinates p -> fprintf fmt "Bad arguments : same coordinates %a" Point.print p
+let print_error fmt = function
+  | Parallel (l1, l2) ->
+      Format.fprintf fmt "Bad arguments : parallel lines %a and %a" print l1
+        print l2
+  | Same_coordinates p ->
+      Format.fprintf fmt "Bad arguments : same coordinates %a" Point.print p
 
 let make a b c =
-  if b = 0. then X(-.c/.a)
-  else Y((-.a/.b),(-.c/.b))
+  if b = 0. then
+    if a = 0. then
+      invalid_arg "Line.make a b c: a and b can not be both equal to 0."
+    else X (-.c /. a)
+  else Y (-.a /. b, -.c /. b)
 
-let make_x f = X(f)
+let make_x f = X f
 
-let make_y a b = Y(a,b)
+let make_y a b = Y (a, b)
 
-let x_axis = Y(0.,0.)
+let x_axis = Y (0., 0.)
 
-let y_axis = X(0.)
+let y_axis = X 0.
 
-let is_vertical = function
-  | X _ -> true
-  | _ -> false
+let is_vertical = function X _ -> true | _ -> false
 
-let is_horizontal = function
-  | Y(0.,_) -> true
-  | _ -> false
+let is_horizontal = function Y (0., _) -> true | _ -> false
 
-let get_coeff = function
-  | Y(a,b) -> (a, -1., b)
-  | X(c) -> (1., 0., -.c)
+let get_coeff = function Y (a, b) -> (a, -1., b) | X c -> (1., 0., -.c)
 
 let to_string = function
-  | X(a) -> ("x="^(string_of_float a))
-  | Y(a,b) -> ("y="^(string_of_float a)^"x+"^(string_of_float b))
+  | X a -> "x=" ^ string_of_float a
+  | Y (a, b) -> "y=" ^ string_of_float a ^ "x+" ^ string_of_float b
 
-let of_points (p1:Point.t) (p2:Point.t) =
-  let open Point in
-  if p1 = p2 then
-    raise (Error (Same_coordinates p1))
-  else if p1.x = p2.x then
-    X(p1.x)
+let of_points (p1 : Point.t) (p2 : Point.t) =
+  if p1 = p2 then raise (Error (Same_coordinates p1))
+  else if p1.x = p2.x then X p1.x
   else
     let coeff = (p2.y -. p1.y) /. (p2.x -. p1.x) in
-    let ord = p1.y -. coeff *. (p1.x)
-    in Y(coeff,ord)
+    let ord = p1.y -. (coeff *. p1.x) in
+    Y (coeff, ord)
 
 let x_from_y l y =
   match l with
-  | X(x) -> x
-  | Y(a,b) when a <> 0.-> (y-.b) /. a
-  | _ -> raise (Error (Parallel(l,Y(0.,y))))
+  | X x -> x
+  | Y (a, b) when a <> 0. -> (y -. b) /. a
+  | _ -> raise (Error (Parallel (l, Y (0., y))))
 
 let y_from_x l x =
   match l with
-  | X(_) -> raise (Error (Parallel(l,(make_x x))))
-  | Y(a,b) -> a *. x +. b
+  | X _ -> raise (Error (Parallel (l, make_x x)))
+  | Y (a, b) -> (a *. x) +. b
 
-let contains (l:t) (p:Point.t) =
-  let open Point in
-  match l with
-  | X(n) -> p.x = n
-  | Y(a,b) -> (a*.(p.x) +. b) = p.y
+let contains (l : t) (p : Point.t) =
+  match l with X n -> p.x = n | Y (a, b) -> (a *. p.x) +. b = p.y
 
-let scale_x (l:t) f =
-  match l with
-  | X(n) -> X(n*.f)
-  | Y(a,b) -> Y(a*.f,b)
+let scale_x (l : t) f =
+  match l with X n -> X (n *. f) | Y (a, b) -> Y (a *. f, b)
 
-let scale_y (l:t) f =
-  match l with
-  | Y(a,b) -> Y (a*.f, b*.f)
-  | _ -> l
+let scale_y (l : t) f =
+  match l with Y (a, b) -> Y (a *. f, b *. f) | _ -> l
 
-let translate dx dy (l:t) =
+let translate dx dy (l : t) =
   match l with
-  | X(n) -> X(n+.dx)
-  | Y(a,b) ->
-    let p1 = Point.translate dx dy (Point.make 0. b)
-    and p2 = Point.translate dx dy (Point.make 1. (a+.b)) in
-    of_points p1 p2
+  | X n -> X (n +. dx)
+  | Y (a, b) ->
+      let p1 = Point.translate dx dy (Point.make 0. b)
+      and p2 = Point.translate dx dy (Point.make 1. (a +. b)) in
+      of_points p1 p2
 
 let parallel l1 l2 =
-  match l1,l2 with
-  | X(_), X(_) -> true
-  | Y(a,_),Y(b,_) -> a = b
+  match (l1, l2) with
+  | X _, X _ -> true
+  | Y (a, _), Y (b, _) -> a = b
   | _ -> false
 
 let intersects l1 l2 = parallel l1 l2 |> not
 
 let intersection l1 l2 =
-  match l1,l2 with
-  | Y(a1,b1),Y(a2,b2) when a1<>a2->
-     let x = (b2 -. b1) /. (a1 -. a2) in
-     let y = a1 *. x +. b1 in
-     Point.make x y
-  | Y(a,b), X(x) | X(x), Y(a,b) ->
-     let y = a *. x +. b in Point.make x y
-  | _  -> raise (Error (Parallel(l1,l2)))
-
+  match (l1, l2) with
+  | Y (a1, b1), Y (a2, b2) when a1 <> a2 ->
+      let x = (b2 -. b1) /. (a1 -. a2) in
+      let y = (a1 *. x) +. b1 in
+      Point.make x y
+  | Y (a, b), X x | X x, Y (a, b) ->
+      let y = (a *. x) +. b in
+      Point.make x y
+  | _ -> raise (Error (Parallel (l1, l2)))
 
 let perpendicular l1 l2 =
-  match l1,l2 with
-  | X(_),Y(0.,_) | Y(0.,_),X(_) -> true
-  | Y(a,_),Y(b,_) when a *. b = -1. -> true
+  match (l1, l2) with
+  | X _, Y (0., _) | Y (0., _), X _ -> true
+  | Y (a, _), Y (b, _) when a *. b = -1. -> true
   | _ -> false
 
 let perpendicular_of_line l p =
   let open Point in
   match l with
-  | Y(0.,_) -> X(p.x)
-  | Y(a,_)  ->
-     let coeff = (-.1.) /. a in
-     let ord = p.y -. coeff *. p.x in
-     Y(coeff,ord)
-  | X(_) -> Y(0.,p.y)
+  | Y (0., _) -> X p.x
+  | Y (a, _) ->
+      let coeff = -1. /. a in
+      let ord = p.y -. (coeff *. p.x) in
+      Y (coeff, ord)
+  | X _ -> Y (0., p.y)
 
 let parallel_of_line l p =
   let open Point in
   match l with
-  | Y(a,_) ->
-     let ord = p.y -. a *. p.x in
-     Y(a,ord)
-  | X(_) -> X(p.x)
+  | Y (a, _) ->
+      let ord = p.y -. (a *. p.x) in
+      Y (a, ord)
+  | X _ -> X p.x
 
-let orth_proj l p =
-  perpendicular_of_line l p |> intersection l
+let orth_proj l p = perpendicular_of_line l p |> intersection l
 
 let point_bissection p1 p2 =
   Point.center p1 p2 |> perpendicular_of_line (of_points p1 p2)
 
-let arbitrary_point l =
-  match l with
-  | X(c) -> Point.make c 0.
-  | Y(_,b) -> Point.make 0. b
+let arbitrary_point = function
+  | X c -> Point.make c 0.
+  | Y (_, b) -> Point.make 0. b
--- src/line.mli
+++ src/line.mli
@@ -1,9 +1,8 @@
 (** This module provides basic operation over the linear equation type *)
 
 type t = private
-	 | X of float
-	 | Y of float * float
-(** the linear equation type *)
+  | X of float
+  | Y of float * float  (** the linear equation type *)
 
 val print : Format.formatter -> t -> unit
 (** printer *)
@@ -11,18 +10,19 @@ val print : Format.formatter -> t -> unit
 val to_string : t -> string
 (** to string *)
 
-type error = | Parallel of t * t
-	           | Same_coordinates of Point.t
-(** the type of errors *)
+type error =
+  | Parallel of t * t
+  | Same_coordinates of Point.t  (** the type of errors *)
 
-exception Error of error
 (** the type of exceptions concerning this module *)
+exception Error of error
 
 val print_error : Format.formatter -> error -> unit
 (** error printer*)
 
 val make : float -> float -> float -> t
-(** [make a b c] builds a line of equation: ax + by + c = 0*)
+(** [make a b c] builds a line of equation: ax + by + c = 0. Raises an error
+    if [a = b = 0.] in which case the equation do not correspond to a line *)
 
 val make_x : float -> t
 (** [make_x a] builds a line of equation: x = a*)
@@ -41,29 +41,28 @@ val of_points : Point.t -> Point.t -> t
     It raises [Error(Same_coordinates)] if [p1 = p2] *)
 
 val is_vertical : t -> bool
-(** [is_vertical l], is [true] if l has an equation of the form: x=cst,
+(** [is_vertical l], is [true] if l has an equation of the form: x=cst, where
+    cst is a constant float*)
+
+val is_horizontal : t -> bool
+(** is_horizontal l, returns true if l has an equation of the form: y=cst,
     where cst is a constant float*)
 
-val is_horizontal: t -> bool
-  (** is_horizontal l, returns true if l has an equation of the form: y=cst,
-      where cst is a constant float*)
-
-val get_coeff : t -> (float*float*float)
-(** returns a tuple (a,b,c) with respect to the equation of line, as: ax + by + c = 0*)
+val get_coeff : t -> float * float * float
+(** returns a tuple (a,b,c) with respect to the equation of line, as: ax + by
+    \+ c = 0*)
 
 val x_from_y : t -> float -> float
-(** 'x_from_y line y', returns 'x', the value on the x-axis corresponding to given 'y' value,
-    with f the affine function associated to 'line', as:
-    f(x) = y
-    raises Parallel if 'line' doesn't intersect the horizontal line going through 'y'
-*)
+(** 'x_from_y line y', returns 'x', the value on the x-axis corresponding to
+    given 'y' value, with f the affine function associated to 'line', as:
+    f(x) = y raises Parallel if 'line' doesn't intersect the horizontal line
+    going through 'y' *)
 
 val y_from_x : t -> float -> float
-(** 'y_from_x line x', returns 'y', the value on the y-axis corresponding to given 'x' value,
-    with f the affine function associated to 'line', as:
-    f(x) = y
-    raises Parallel if 'line' doesn't intersect the vertical line going through 'x'
-*)
+(** 'y_from_x line x', returns 'y', the value on the y-axis corresponding to
+    given 'x' value, with f the affine function associated to 'line', as:
+    f(x) = y raises Parallel if 'line' doesn't intersect the vertical line
+    going through 'x' *)
 
 val contains : t -> Point.t -> bool
 (** contains l1 p returns true if l1goes through p. false otherwise.*)
@@ -81,25 +80,27 @@ val intersects : t -> t -> bool
 (** intersects l1 l2 returns true if l1 and l2 intersects. false otherwise.*)
 
 val intersection : t -> t -> Point.t
-(** intersection l1 l2 returns the point at the intersection of l1 and l2.
-    It raises Error(Parralel) if l1 and l2 dont intersect *)
+(** intersection l1 l2 returns the point at the intersection of l1 and l2. It
+    raises Error(Parralel) if l1 and l2 dont intersect *)
 
 val perpendicular : t -> t -> bool
-(** perpendicular l1 l2 returns true if l1 and l2 are perpendicular.
-    false otherwise.*)
+(** perpendicular l1 l2 returns true if l1 and l2 are perpendicular. false
+    otherwise.*)
 
 val perpendicular_of_line : t -> Point.t -> t
-(** perpendicular_of_line l p  returns the line perpendicular to l that goes through p.*)
+(** perpendicular_of_line l p returns the line perpendicular to l that goes
+    through p.*)
 
 val parallel_of_line : t -> Point.t -> t
-(** parallel_of_line l p  returns the line parallel to l that goes through p.*)
+(** parallel_of_line l p returns the line parallel to l that goes through p.*)
 
 val orth_proj : t -> Point.t -> Point.t
 (** orth_proj l p, returns the orthogonal projection of p on l*)
 
 val point_bissection : Point.t -> Point.t -> t
-(** [point_bissection p1 p2] builds the line [l] that bissects the
-   segment p1p2 in its center. It Raises [Error(Same_coordinates)] if [p1 = p2] *)
+(** [point_bissection p1 p2] builds the line [l] that bissects the segment
+    p1p2 in its center. It Raises [Error(Same_coordinates)] if [p1 = p2] *)
 
 val arbitrary_point : t -> Point.t
-(** [arbitrary_point l] chooses an aribitrary point [p] such that [contains l p] is true *)
+(** [arbitrary_point l] chooses an aribitrary point [p] such that
+    [contains l p] is true *)
